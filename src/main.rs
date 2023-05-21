12e880faf6759f8e870458abf54d51bc88e849ae
dhodgdon <hod20010@byui.edu>
2023-05-20 22:31:28 -0700

    Added a Rocket web app.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
+    // thread,
+    // time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); 
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(10);
 
-    for stream in listener.incoming().take(6) { 
+    for stream in listener.incoming() // .take(6) 
+    { 
         let stream = stream.unwrap();
         
         pool.execute(|| {
@@ -28,10 +29,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello_world.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello_world.html")
-        }
+        // "GET /sleep HTTP/1.1" => {
+        //     thread::sleep(Duration::from_secs(5));
+        //     ("HTTP/1.1 200 OK", "hello_world.html")
+        // }
+        // "GET /hidden HTTP/1.1" => ("HTTP/1.1 200 OK", "hidden.html"),\
+        "GET /text_counter HTTP/1.1" => ("HTTP/1.1 200 OK", "text_counter.html"),
         "GET /hidden HTTP/1.1" => ("HTTP/1.1 200 OK", "hidden.html"),
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
@@ -46,3 +49,33 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
+
+
+
+
+// ########## Rocket Web App - Uncomment to Use ############
+// #[macro_use] extern crate rocket;
+
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello World!"
+// }
+
+// #[get("/hello/<name>")]
+// fn hello(name: &str) -> String {
+//     format!("Hello {}!", name)
+// }
+
+// #[get("/add/<num1>/<num2>")]
+// fn add(num1: i64, num2: i64) -> String {
+//     let new_num = num1 + num2;
+//     format!("{} + {} = {}", num1, num2, new_num)
+// }
+
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build()
+//         .mount("/", routes![index])
+//         .mount("/", routes![hello])
+//         .mount("/", routes![add])
+// }
